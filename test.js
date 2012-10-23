1e65fa12eddc6fc9a9cae4aacff2dd2ea8721e6d
Brian Graham <drakain@gmail.com>
2012-10-23 17:14:42 -0400

    Basic structure of how tests ought to work.

--- test.js
+++ test.js
@@ -4,17 +4,22 @@
 var Test = (function(){
 
    var test = function(){
+       var self=this;
+       self.data = {}; //TODO: will hold a hash of things to check
    }
 
-   test.prototype.begin = function(){
+   test.prototype.begin = function(testName){
+      //TODO: change mode of attaching.
 
    };
-   test.prototype.criteria = function(){
 
+   test.prototype.criteria = function(testCall, expectedResult, name ){
+       //TODO: attach params to a prop list
    };
-   test.prototype.execute = function(){
 
-   };
+   test.prototype.run = function(){
+      //TODO: run through list, run checks, output results
+   }
 
    return test;
 
