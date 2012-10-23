6ccd6d6e33968696ae8dc99bdc19f400173a66cb
Brian Graham <drakain@gmail.com>
2012-10-23 17:00:45 -0400

    fib to 4mil.

--- 002/alpha.js
+++ 002/alpha.js
@@ -2,10 +2,38 @@
  * First attempt at solving the problem
  **/
 var P002 = (function(){
-    
-    var fib = function(n){
-        return fib();
-    }
 
+    var fib = function(n){
+        var self=this;
+    };
+    
+    fib.prototype.fib = function(n){
+        var self=this;
+        var series = self.generateSeries(n);
+        var sum = self.sum(series);
+        return sum;
+    };
+
+    fib.prototype.generateSeries = function(limit) {
+        var self=this;
+        var series = [1, 2];
+        do {
+            series.push(series[series.length-1] + series[series.length-2]);
+        } while (series[series.length-1]<limit);
+        series.pop();
+        
+        return series;
+    };
+
+    fib.prototype.sum = function(list){
+      var self=this;
+      var sum=0;
+      for(var i=0;i<list.length;i++){
+        sum+=list[i];
+      }
+      return sum;
+    };
+
+    return fib;
 }());
 
---
+++ 002/tests.js
@@ -0,0 +1,10 @@
+
+var fib = new P002();
+
+print("generateSeries", fib.generateSeries(10));
+print("sum", fib.sum([1,2,3,5,8]));
+print("fib to 10: ", fib.fib(10));
+print();
+print("fib to 4 000 000: ", fib.fib(4000000));
+
+
