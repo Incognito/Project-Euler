c0dca8e31e70755f34b17cc6f860af9e356a1946
Brian Graham <drakain@gmail.com>
2012-10-23 07:28:11 -0400

    I want a really small testing library, so lets just make one...

---
+++ test.js
@@ -0,0 +1,22 @@
+/*
+ * Really small testing tool
+ **/
+var Test = (function(){
+
+   var test = function(){
+   }
+
+   test.prototype.begin = function(){
+
+   };
+   test.prototype.criteria = function(){
+
+   };
+   test.prototype.execute = function(){
+
+   };
+
+   return test;
+
+}());
+
