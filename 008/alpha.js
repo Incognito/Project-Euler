a7cb9ee20c19d85bb433623bbe123c4a715142f5
Brian Graham <drakain@gmail.com>
2012-11-12 16:40:10 -0500

    Setup for 008

---
+++ 008/alpha.js
@@ -0,0 +1,12 @@
+var P008 = (function(){
+
+    var productNumber = function(){
+        var self=this;
+    };
+    
+    productNumber.prototype.find = function(foo) {
+    };
+
+    return productNumber;
+}());
+
